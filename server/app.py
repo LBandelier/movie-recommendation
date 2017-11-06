37ba6cd2f4ed102908115cb4c9c901a5c668d93f
LouisBandelier <louisbandelier@MacBook-Pro-de-Louis.local>
2017-11-06 12:36:52 -0500

    Liste Films notés + Bootstrap simple

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -7,4 +9,7 @@ def hello_world():
 
 @app.route('/my-movies')
 def list_user_movies():
-    return render_template('my-movies.html', page_title='My Movies')
+    my_movies_rated = pd.read_csv('./data/my-ratings.csv', sep=',', header=0)
+    all_movies = pd.read_csv('./data/movies.csv', sep=',', header=0)
+    my_movies_rated = pd.merge(my_movies_rated, all_movies, on='movieId', how='inner')
+    return render_template('my-movies.html', page_title = "My Movies", movies = my_movies_rated)
